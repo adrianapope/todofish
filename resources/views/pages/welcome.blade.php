80ca7f5b37cc35937efedcd1c0733167565a76d1
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-28 23:52:36 +0800

    added text to landing page and fish photo

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,17 +6,22 @@
 	<div class="about-wrapper">
 		<div class="row">
 			<div class="col-sm-6">
-				<h1>Become a todo fish.</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat adipisci iusto, sapiente cupiditate quo commodi, omnis? Provident fuga inventore explicabo laudantium distinctio. Culpa rem autem odit, iure necessitatibus facere tempora.
+				<h1>Become a ToDo fish.</h1>
+					<p>Swimming in tasks? If you need somewhere to put down your thoughts, ToDoFish has got you covered! Organize all your tasks in one easy place. Create, update, and delete tasks on the fly. Keep track of tasks you've already completed and those that are still active with one simple click.
 					</p>
-				</div>
 			</div>
+		</div>
 
+		<div class="row">
+			<div class="col-sm-4">
+				<a href="/auth/register" class="btn btn-info btn-lg">Sign Up Today</a>
+			</div>
+		</div>
+
+		<div class="404-photo-wrapper">
 			<div class="row">
-				<div class="col-sm-4">
-					<a href="/auth/register" class="btn btn-info btn-lg">Sign Up Today</a>
-					<p>Already a member?</p>
-					<a href="/auth/login" class="btn btn-primary btn-lg">Login</a>
+				<div class="col-sm-6">
+					<img src="/img/fishy.png" alt="orange fish" class="img-rounded img-responsive">
 				</div>
 			</div>
 		</div>
