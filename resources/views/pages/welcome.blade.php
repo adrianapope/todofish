1edd694e9b4464fd89e51b6f29bc66a569ed5d54
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-28 23:26:45 +0800

    Added landing page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,7 +2,24 @@
 
 @section('content')
 
-<h1>Best task manager ever!</h1>
-homepage content
+<div class="container">
+	<div class="about-wrapper">
+		<div class="row">
+			<div class="col-sm-6">
+				<h1>Become a todo fish.</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat adipisci iusto, sapiente cupiditate quo commodi, omnis? Provident fuga inventore explicabo laudantium distinctio. Culpa rem autem odit, iure necessitatibus facere tempora.
+					</p>
+				</div>
+			</div>
 
+			<div class="row">
+				<div class="col-sm-4">
+					<a href="/auth/register" class="btn btn-info btn-lg">Sign Up Today</a>
+					<p>Already a member?</p>
+					<a href="/auth/login" class="btn btn-primary btn-lg">Login</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
