307a5ffdbac54da988fb7ce3184c4cff3b451281
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-28 23:34:57 +0800

    deleted file

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+@section('content')
+
+<div class="container">
+	<div class="404-wrapper">
+		<div class="row">
+			<div class="col-sm-6">
+				<h1>Oops!
+				The page you've requested could not be found.
+				</h1>
+			</div>
+			<div class="404-photo-wrapper">
+				<img src="/img/fishbowl.png" alt="fishbowl" class="img-rounded img-responsive">
+			</div>
+		</div>
+	</div>
+</div>
+@stop
