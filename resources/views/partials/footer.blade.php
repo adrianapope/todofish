6967d2ed609028a51a6ced7b6c0e60653e0e957b
Adriana Pope <adrianagiancarla@gmail.com>
2015-09-29 20:01:00 +0800

    tweaked footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+	<div class="container">
+		<div class="footer-wrapper">
+			<div clas="row">
+				<div class="col-sm-12">
+					<ul>
+						<li><a href="/pages/about">About</a></li>
+						<li><a href="/pages/contact">Contact</a></li>
+						<li><a href="/pages/faq">FAQ</a></li>
+						<li>&copy; 2015 ToDoFish</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
